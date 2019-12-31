ef025dddb44f863659dfa5b9458136ad0c6a517a
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2019-12-31 15:32:12 -0800

    Create spam.js

---
+++ spam.js
@@ -0,0 +1,8 @@
+var to = prompt("To: ");
+document.getElementById("to").value = to;
+
+var subject = prompt("Subject: ");
+document.getElementsByName('subject').value = subject;
+
+var body = prompt('Body: ');
+document.getElementsByName('body').value = body;
